53ea08c7355bd547a1fa090510398f8866cc2ac8
Sam Porter Giffords <travelinsamp@gmail.com>
2018-03-25 21:43:07 -0700

    Local storage now working

--- js/app.js
+++ js/app.js
@@ -1,26 +1,16 @@
 'use strict'
 
-// alert("Can you see this ?");
-// debugger;
-
-/*
-// STRETCH GOAL
-// https://www.w3schools.com/howto/howto_js_tabs.asp
-
-*/
-// DECLARE ALL GLOBAL VARIABLES
-
 // ARRAY TO HOLD ALL PRODUCT OBJECTS
-Product.allProducts = [];             // used on line 70 in object constructor
+Product.allProducts = [];
 
 // array to keep track of previously displayed images
-var lastDisplayed = [];           // used on lines 139 - 141
+var lastDisplayed = [];
 
 // product names for bar chart labels
-var productNames = [];                // used on line 69 w/in constructor
+var productNames = [];
 
 // product votes for bar chart data
-var productClicks = [];               // used on line 227
+var productClicks = [];
 
 // click tracker
 Product.totalClicks = 0;
@@ -30,11 +20,6 @@ var totalGuesses = 25;
 var img1 = document.getElementById("prod-pic1");
 var img2 = document.getElementById("prod-pic2");
 var img3 = document.getElementById("prod-pic3");
-
-// access the image elements from the DOM
-// var rightImg = document.getElementById('right');
-// var midImg = document.getElementById('mid');
-// var leftImg = document.getElementById('left');
 
 // access the section element from the DOM
 var sectionElement = document.getElementById('prod-images');
@@ -46,54 +31,91 @@ var unorderedListElement = document.getElementById('results');
 function Product(filepath, imgname) {
   this.filepath = filepath;
   this.imgname = imgname;
-  this.numDisplayed = 0;          // numberTimesViewed
-  this.numClicked = 0;            // numberTimesClicked
+  this.numDisplayed = 0;
+  this.numClicked = 0;
   // this.productLine = productline;
   productNames.push(this.imgname);
   Product.allProducts.push(this);
 }
 
-// SET UP ALL ARGUMENTS FOR NEW OBJECTS
-// New instances of products
-new Product('img/bag.jpg', 'R2D2 Bag' /*, 'luggage' */);
-new Product('img/banana.jpg', 'Banana slicer', 'kitchen');
-new Product('img/bathroom.jpg', 'Tablet and TP Holder', 'bathroom');
-new Product('img/boots.jpg', 'Yellow Boots with no Toes', 'shoes');
-new Product('img/breakfast.jpg', 'All In One Toaster', 'kitchen');
+function setupProducts() {
 
-new Product('img/bubblegum.jpg', 'Meatball Bubble Gum', 'edible');
-new Product('img/chair.jpg', 'Chair with Convex Seat', 'furniture');
-new Product('img/cthulhu.jpg', 'Cthulhu Figurine', 'toy');
-new Product('img/dog-duck.jpg', 'Dog with Duck Beak', 'pets');
-new Product('img/dragon.jpg', 'Dragon Meat in a Can', 'edible');
+  var productsAsString = localStorage.getItem('allProducts');
+  var lastDisplayedAsString = localStorage.getItem('lastDisplayed');
+  var productNamesAsString = localStorage.getItem('productNames');
 
-new Product('img/pen.jpg', 'Utensil Pen', 'kitchen');
-new Product('img/pet-sweep.jpg', 'Pet Cleaning Slippers', 'pets');
-new Product('img/scissors.jpg', 'Pizza Scissors', 'kitchen');
-new Product('img/shark.jpg', 'Shark Sleeping Bag', 'soft-goods');
-new Product('img/sweep.png', 'Baby Onesie Sweeper', 'kids');
+  var useableProducts = JSON.parse(productsAsString);
+  var useableLastDisplayed = JSON.parse(lastDisplayedAsString);
+  var useableProductNames = JSON.parse(productNamesAsString);
 
-new Product('img/tauntaun.jpg', 'Tauntaun Sleeping Bag', 'soft-goods');
-new Product('img/unicorn.jpg', 'Unicorn Meat in a Can', 'edible');
-new Product('img/usb.gif', 'USB Tentacle', 'tech');
-new Product('img/water-can.jpg', 'Recursive Watering Can', 'gardening');
-new Product('img/wine-glass.jpg', 'Abnormal Wine Glass', 'kitchen');
+  // Check local storage for stored values
+  if (useableProducts && useableProducts.length) {
+    Product.allProducts = useableProducts;
+
+  } else {
+
+    // New product arguments to create instances   
+    new Product('img/bag.jpg', 'R2D2 Bag' /*, 'luggage' */);
+    new Product('img/banana.jpg', 'Banana slicer', 'kitchen');
+    new Product('img/bathroom.jpg', 'Tablet and TP Holder', 'bathroom');
+    new Product('img/boots.jpg', 'Yellow Boots with no Toes', 'shoes');
+    new Product('img/breakfast.jpg', 'All In One Toaster', 'kitchen');
+
+    new Product('img/bubblegum.jpg', 'Meatball Bubble Gum', 'edible');
+    new Product('img/chair.jpg', 'Chair with Convex Seat', 'furniture');
+    new Product('img/cthulhu.jpg', 'Cthulhu Figurine', 'toy');
+    new Product('img/dog-duck.jpg', 'Dog with Duck Beak', 'pets');
+    new Product('img/dragon.jpg', 'Dragon Meat in a Can', 'edible');
+
+    new Product('img/pen.jpg', 'Utensil Pen', 'kitchen');
+    new Product('img/pet-sweep.jpg', 'Pet Cleaning Slippers', 'pets');
+    new Product('img/scissors.jpg', 'Pizza Scissors', 'kitchen');
+    new Product('img/shark.jpg', 'Shark Sleeping Bag', 'soft-goods');
+    new Product('img/sweep.png', 'Baby Onesie Sweeper', 'kids');
+
+    new Product('img/tauntaun.jpg', 'Tauntaun Sleeping Bag', 'soft-goods');
+    new Product('img/unicorn.jpg', 'Unicorn Meat in a Can', 'edible');
+    new Product('img/usb.gif', 'USB Tentacle', 'tech');
+    new Product('img/water-can.jpg', 'Recursive Watering Can', 'gardening');
+    new Product('img/wine-glass.jpg', 'Abnormal Wine Glass', 'kitchen');
+  }
+
+  // Check local storage for stored values
+  if (useableLastDisplayed && useableLastDisplayed.length) {
+    lastDisplayed = useableLastDisplayed;
+
+  } else {
+    lastDisplayed = [];
+  }
+
+  // Check local storage for stored values
+  if (useableProductNames && useableProductNames.length) {
+    productNames = useableProductNames;
+
+  } else {
+    productNames = [];
+    for (var i = 0; i < Product.allProducts.length; i++) {
+      productNames.push(Product.allProducts[i].imgname);
+    }
+  }
+}
 
 // access the element from the DOM
-// when accessing from the DOM, var keyword is necessary
 var imgElement1 = document.getElementById('prod-pic1');
 var imgElement2 = document.getElementById('prod-pic2');
 var imgElement3 = document.getElementById('prod-pic3');
 
-
 // add event listener
-//sectionElement.addEventListener('click', clickHandler);
 imgElement1.addEventListener('click', clickHandler);
 imgElement2.addEventListener('click', clickHandler);
 imgElement3.addEventListener('click', clickHandler);
 
 // randomly display three pictures
 function randomProduct() {
+  if (Product.totalClicks == totalGuesses) {
+    renderChart();
+    return;
+  }
 
   // generate two random indices
   var randomLeft = Math.floor(Math.random() * Product.allProducts.length);
@@ -107,15 +129,12 @@ function randomProduct() {
     || lastDisplayed.includes(randomMid)
     || lastDisplayed.includes(randomRight)) {
 
-    console.log('Duplicate was caught here');
-
     randomLeft = Math.floor(Math.random() * Product.allProducts.length);
     randomMid = Math.floor(Math.random() * Product.allProducts.length);
     randomRight = Math.floor(Math.random() * Product.allProducts.length);
   }                 // END OF WHILE LOOP
 
-  // Now that we know they are unique numbers, display the three
-  // unique images on the screen
+  // Display the three unique images on the screen
   imgElement1.src = Product.allProducts[randomLeft].filepath;
   imgElement1.alt = Product.allProducts[randomLeft].imgname;
 
@@ -125,14 +144,12 @@ function randomProduct() {
   imgElement3.src = Product.allProducts[randomRight].filepath;
   imgElement3.alt = Product.allProducts[randomRight].imgname;
 
-  // incremented the number of times displayed
+  // increment the number of times displayed
   Product.allProducts[randomLeft].numDisplayed++;
   Product.allProducts[randomMid].numDisplayed++;
   Product.allProducts[randomRight].numDisplayed++;
 
   // keep track of previously displayed images
-  // APPROACH 1:
-
   // create array to hold displayed items
   lastDisplayed = [];
 
@@ -140,98 +157,93 @@ function randomProduct() {
   lastDisplayed.push(randomLeft);
   lastDisplayed.push(randomMid);
   lastDisplayed.push(randomRight);
-}                     // END OF randomProduct FUNCTION
+}
 
 // CREATE EVENT TO HANDLE CLICK ON IMAGE
 function clickHandler(event) {
-  // increment click counter
   Product.totalClicks++;
-  // increment clicks (votes) on the specific image (numClicked)  
-  console.log(event.target);
+
 
   // use a for loop to determine which product image was actually clicked on
   for (var i in Product.allProducts) {
-
     if (event.target.alt === Product.allProducts[i].imgname) {
-
-      // once we find the one that matches, increment counter on 
-      // that specific object
+      // If it matches, increment counter on specific object
       Product.allProducts[i].numClicked++;
     }
-  }                                 // END CLICK HANDLER FUNCTION
-
+  }
 
   //  CLICK COUNTER
-  //  Check the click counter
   if (Product.totalClicks < totalGuesses) {
-
-    randomProduct();             // commented out to test
-
+    randomProduct();
   } else {
 
-    // turn off event listener
-    //sectionElement.removeEventListener('click', clickHandler);
+    finish();
+
+  }
+}
+
+function toggleEventListeners() {
+  if (Product.totalClicks >= totalGuesses) {
+    // turn off event listener if totalGuesses maxed
     imgElement1.removeEventListener('click', clickHandler);
     imgElement2.removeEventListener('click', clickHandler);
     imgElement3.removeEventListener('click', clickHandler);
-
-    // if less than 24, display more products
-    showResults();
-
-    // display the fully populated chart
-    renderChart();
-    // if less than 24, display a new set of product images
-
+  } else {
+    imgElement1.addEventListener('click', clickHandler);
+    imgElement2.addEventListener('click', clickHandler);
+    imgElement3.addEventListener('click', clickHandler);
   }
-}                         // END CLICK COUNTER IF STATEMENT
+}
 
-
-//////////////////////////////////////////////
-// CREATE FUNCTION TO DISPLAY RESULTS TO WEBPAGE
+// DISPLAY RESULTS TO WEBPAGE
 function showResults() {
-  console.log("You clicked " + Product.totalClicks + " times !");
 
-  // CREATE SECONDARY FUNCTION TO UPDATE VOTECLICKS FOR PRODUCT
+
+  // UPDATE VOTECLICKS FOR PRODUCT
   function updateClicks() {
     for (var i in Product.allProducts) {
-      // APPROACH 1:
-      // goatVotes.push(Goat.allGoats[i].votes);
-      // APPROACH 2:
       productClicks[i] = Product.allProducts[i].votes;
     }
   }
 }
 
-// RENDER IMAGE ON PAGE LOAD (CALL FUNCTION)
-randomProduct();
-
-////////////////////////////////////////////////////////////
+function clearImages() {
+  document.getElementById('prod-images').innerHTML = '';
+}
 
 // use Chart.js to create a bar chart
 function renderChart() {
-  // access the canvas element from the DOM using a var
-  // var context = document.getElementById('goat-chart').getContext('2d');
-  var context = document.getElementById('myChart');
+  clearImages();
+  // access the canvas element from the DOM 
+  productClicks = [];
+  for (var i = 0; i < Product.allProducts.length; i++) {
+    productClicks.push(Product.allProducts[i].numClicked)
+  }
 
-  var arrayOfColors = ['#eeeeee', '#111111', '#525252', '#eeeeee', '#111111', '#525252', '#eeeeee', '#111111', '#525252', '#eeeeee', '#111111', '#525252', '#eeeeee', '#111111', '#525252', '#eeeeee', '#111111', '#525252', '#eeeeee', '#111111', '#525252', '#eeeeee', '#111111', '#525252', '#eeeeee'];
+  var context = document.getElementById('myChart').getContext('2d');
 
+  var arrayOfColors = [
+    '#378888', '#375F88', '#373788', '#5F3788', '#88375F',
+    '#886337', '#848837', '#5C8837', '#37883A', '#378863',
+    '#378888', '#375F88', '#373788', '#5F3788', '#88375F',
+    '#886337', '#848837', '#5C8837', '#37883A', '#378863'];
 
   var productChart = new Chart(context, {
     type: 'bar',
     data: {
-      labels: productNames, // array of product names, populated above in Global Variable section
-      datasets: [
-        {
-          label: "Clicks on Product",
-          data: productClicks,
-          backgroundColor: arrayOfColors,
-        }]
-      // labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"]
+      labels: productNames,
+      datasets: [{
+        label: "Clicks on Product",
+        data: productClicks,
+        backgroundColor: arrayOfColors
+      }]
+
     },
     options: {
       scales: {
         yAxes: [{
           ticks: {
+            stepSize: 1,
             beginAtZero: true
           }
         }]
@@ -241,8 +253,51 @@ function renderChart() {
 
 }
 
+function finish() {
+  // Save to local storage
+  var saveProducts = JSON.stringify(Product.allProducts);
+  var saveLastDisplayed = JSON.stringify(lastDisplayed);
+  var saveProductNames = JSON.stringify(productNames);
+  var saveProductClicks = JSON.stringify(productClicks);
+  var saveClickCount = JSON.stringify(Product.totalClicks);
 
+  localStorage.setItem('allProducts', saveProducts);
+  localStorage.setItem('lastDisplayed', saveLastDisplayed);
+  localStorage.setItem('productNames', saveProductNames);
+  localStorage.setItem('productClicks', saveProductClicks);
+  localStorage.setItem('clickCount', saveClickCount);
 
+  toggleEventListeners();
+  renderChart();
+}
 
+// Get values from local storage if already stored
+function setupCounters() {
+  var productClicksAsString = localStorage.getItem('productClicks');
+  var clickCountAsString = localStorage.getItem('clickCount');
 
+  var savedProductClicks = JSON.parse(productClicksAsString);
 
+  if (savedProductClicks && savedProductClicks.length) {
+    productClicks = savedProductClicks;
+
+  } else {
+    for (var i = 0; i < Product.allProducts.length; i++) {
+      productClicks.push(0);
+    }
+  }
+
+  var savedClickCount = JSON.parse(clickCountAsString);
+  if (savedClickCount) {
+    Product.totalClicks = savedClickCount;
+
+  } else {
+    Product.totalClicks = 0;
+  }
+}
+
+// RENDER IMAGE ON PAGE LOAD 
+setupProducts();
+setupCounters();
+toggleEventListeners();
+randomProduct();
--- user_stories.md
+++ user_stories.md
@@ -1,6 +1,8 @@
 
 
 
+user_stories.md: Express your stories in story language (as a so and so I need a thingamabob so that I can do stuff with it)
+
 =============================================================
 
 Part of your assignment today is to write your own user stories. Be sure to consider the multiple roles involved: the marketing research team, the developer, and the focus group participant who will be using the application. Try to write 4-5 user stories for each role. DO THIS STEP FIRST in a file called user_stories.md. The commit logs in your repo will have a first couple of commits for the scaffolding process, but next you should have a 'user stories' commit that is in place before any code is written.
@@ -9,26 +11,22 @@ Marketing Research Team:
 
 As the marketing director, I need a quick way to tell which products have 
 the most number of clicks.
-As the marketing director, I need to be able to show my boss a chart that summarizes those results.
-As the marketing director, I want this information in an organized container and I prefer a chart format.
-As a designer, I want the page displayed using the style sheet it took me 3 years to fully create - fonts, colors, and general "feel".
-As the market research analyst, I want to understand what might drive customers to purchase products in the future.
-
+As the marketing director, I want this information displayed to me in an easy-to-read format -- like a chart.   
+As the designer, I want the page displayed using the style sheet it took me 3 years to completely style - fonts, colors, and general "feel".
+As the marketing research analyst, I want better insight into what my customer might want to see in future product offerings. 
+As the product manager, I want to drive customers to purchase products in the future.
+As the sales director working with the marketing team, I want the customer to have an easy way to click through to get to an order form and shopping cart.
 
 Developer:
-** wants to write elegant and DRY code
-** wants to provide functionality in the form of logic to end up with accurate information
-** wants to give a clear path to anyone who needs to follow code in the future when s/he is promoted to Senior developer !
-** wants to have no conflict with the marketing director re: styles so that s/he is hired for the next project.
+As a developer, I want to write elegant and DRY code.
+As a developer, I want to provide functionality in the form of logic to end up with accurate information.
+As a developer, I want to give a clear path in the form of useful comments to anyone who needs to follow code in the future when I am promoted.
+As a developer, I want to have no conflict with the marketing director re: styles so that I can be hired for the next project.
 
 Focus Group Participant:  
-** wants a neatly organized page so that it's clear what is being asked
-** wants to click through the products as quickly as possible so that s/he collects his/her $10 bucks and/or gets done with his/her community service requirement for college admission
-** wants to have to go through as few iterations as possible because clicking through this stuff is fairly boring, except when UNICORN MEAT shows up in the list.
-** wants to see clear and easy-to-see images of all the products each iteration
-
-
-
-
-
+As a focus group participant, I want a neatly organized page so that it's clear what is being asked of me.
+As a focus group participant, I want to click through the products as quickly as possible so that I can collect my $10 bucks and/or get done with this community service requirement for college admission.
+As a focus group participant, I want to have to go through as few iterations as possible because clicking through this stuff is fairly boring, except when UNICORN MEAT shows up in the list.
+As a focus group participant, I want to see clear and easy-to-see images of all the products each iteration.
+As a focus group participant, I want everything to work the very first time.  If the page errors out, I will not be happy.  
 
